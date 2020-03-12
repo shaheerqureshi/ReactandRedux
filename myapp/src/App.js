f39069d8ad024923b0641dcbf9a2b6b20f20ab62
shaheerqureshi <shaheerqureshi334@gmail.com>
2020-03-12 19:13:06 +0500

    updated

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,24 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import  Ninjas from "./Ninjas";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>My first react App</h1>
+     <p>Welcome</p>
+     <Ninjas name="shaheer" age="23" belt="black"/>
     </div>
   );
 }
